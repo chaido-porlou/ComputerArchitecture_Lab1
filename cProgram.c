ba2994ffc4d2e9103e5b5bcac1bc0098c3a9f260
chaido-porlou <74879115+chaido-porlou@users.noreply.github.com>
2020-11-23 13:14:29 +0200

    Add files via upload

---
+++ cProgram.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+	int i = 0;
+	for (i=0;i<3;i++){
+		printf("My number is: %d \n",i);
+	}
+	printf("I can count from 0 to 2!\nGreat Success!\n\n");
+
+	return 0;
+}
